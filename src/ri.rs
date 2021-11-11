b4f8f1f94fb71cf0f580e9d010214829e0da7928
Brad Gibson <b2b@humanenginuity.com>
2021-11-11 14:53:56 -0800

    impl `Sub`: green

--- src/ri.rs
+++ src/ri.rs
@@ -1,7 +1,7 @@
-#[cfg(test)]
-mod unit_tests;
+mod add;
+mod sub;
+
 use crate::consts::*;
-use std::ops::Add;
 
 macro_rules! impl_ranged_inclusive {
     ($($SrcTL:ident $SrcTU:ident),+) => {
@@ -32,29 +32,6 @@ macro_rules! impl_ranged_inclusive {
                     self.value
                 }
             }
-
-            // ∵ range bounds are always const, use of `unwrap()` in `const` context does not panic, but rather halts
-            // compilation in the event of overflow.  This is exactly the behavior we want to ensure compile-time
-            // verification of bounds and zero runtime overhead.
-            // See https://stackoverflow.com/a/66362584/1541330 for an explanation of why the `where` clause is
-            // necessary.
-            #[allow(clippy::unwrap_used)]
-            impl<const START_LHS: $SrcTL, const END_LHS: $SrcTL, const START_RHS: $SrcTL, const END_RHS: $SrcTL>
-                Add<$SrcTU<START_RHS, END_RHS>> for $SrcTU<START_LHS, END_LHS>
-            where
-                $SrcTU<{ START_LHS.checked_add(START_RHS).unwrap() }, { END_LHS.checked_add(END_RHS).unwrap() }>: Sized,
-            {
-                type Output =
-                    $SrcTU<{ START_LHS.checked_add(START_RHS).unwrap() }, { END_LHS.checked_add(END_RHS).unwrap() }>;
-
-                fn add(self, rhs: $SrcTU<START_RHS, END_RHS>) -> Self::Output {
-                    // Given `value` is range-checked for both lhs & rhs, ∵ the sum of the bounds does not overflow,
-                    // then the sum of the `value`s cannot overflow either.  Elide bounds checking.
-                    #[allow(unsafe_code)]
-                    Self::Output { value: unsafe { self.value().unchecked_add(rhs.value()) } }
-                }
-            }
-
         )+
     }
 }
