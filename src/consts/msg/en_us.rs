3c1539ef54b668d7c3740fa74a923dad6f391fac
Brad Gibson <b2b@humanenginuity.com>
2021-11-11 12:29:55 -0800

    unchecked `Add` with typestate/compile-time enforced bounds: green

--- src/consts/msg/en_us.rs
+++ src/consts/msg/en_us.rs
@@ -3,3 +3,4 @@ pub const ERR_INVALID_RANGE_INCLUSIVE_BOUNDS: &str =
 pub const ERR_VALUE_OUT_OF_INCLUSIVE_BOUNDS: &str =
     "Error: Value outside of (inclusive) range bounds. (`START` must be <= `value` <= `END`)";
 pub const DOES_NOT_CONTAIN: &str = "does not contain";
+pub const ERR_START_BOUND_NOT_LE_END_BOUND: &str = "Error: `START` bound must be <= `END` bound.";
--- src/ri.rs
+++ src/ri.rs
@@ -1,3 +1,8 @@
+#[cfg(test)]
+mod unit_tests;
+use crate::consts::*;
+use std::ops::Add;
+
 macro_rules! impl_ranged_inclusive {
     ($($SrcTL:ident $SrcTU:ident),+) => {
         $(
@@ -7,7 +12,8 @@ macro_rules! impl_ranged_inclusive {
             }
 
             impl<const START: $SrcTL, const END: $SrcTL> $SrcTU<START, END> {
-                const _INVARIANT_START_LE_END: () = assert!(START <= END);
+                #[allow(dead_code)]
+                const INVARIANT_START_LE_END: () = assert!(START <= END, "{}", msg::ERR_START_BOUND_NOT_LE_END_BOUND);
 
                 /// Constructor
                 /// Returns `Some(Self)` when `value` is within bounds or `None` otherwise.
@@ -20,7 +26,35 @@ macro_rules! impl_ranged_inclusive {
                         false => None,
                     }
                 }
+
+                #[must_use]
+                pub const fn value(&self) -> $SrcTL {
+                    self.value
+                }
             }
+
+            // ∵ range bounds are always const, use of `unwrap()` in `const` context does not panic, but rather halts
+            // compilation in the event of overflow.  This is exactly the behavior we want to ensure compile-time
+            // verification of bounds and zero runtime overhead.
+            // See https://stackoverflow.com/a/66362584/1541330 for an explanation of why the `where` clause is
+            // necessary.
+            #[allow(clippy::unwrap_used)]
+            impl<const START_LHS: $SrcTL, const END_LHS: $SrcTL, const START_RHS: $SrcTL, const END_RHS: $SrcTL>
+                Add<$SrcTU<START_RHS, END_RHS>> for $SrcTU<START_LHS, END_LHS>
+            where
+                $SrcTU<{ START_LHS.checked_add(START_RHS).unwrap() }, { END_LHS.checked_add(END_RHS).unwrap() }>: Sized,
+            {
+                type Output =
+                    $SrcTU<{ START_LHS.checked_add(START_RHS).unwrap() }, { END_LHS.checked_add(END_RHS).unwrap() }>;
+
+                fn add(self, rhs: $SrcTU<START_RHS, END_RHS>) -> Self::Output {
+                    // Given `value` is range-checked for both lhs & rhs, ∵ the sum of the bounds does not overflow,
+                    // then the sum of the `value`s cannot overflow either.  Elide bounds checking.
+                    #[allow(unsafe_code)]
+                    Self::Output { value: unsafe { self.value().unchecked_add(rhs.value()) } }
+                }
+            }
+
         )+
     }
 }
